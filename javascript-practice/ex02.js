3d76ab853d02585a1f72729ef58fb1be62902e6d
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-25 15:07:55 +0900

    js ex01

---
+++ javascript-practice/ex02.js
@@ -0,0 +1,19 @@
+/*
+변수와 데이터 타입
+
+- [기본타입(primitive type), 유사객체]
+1. number
+2. boolean
+3. string
+4. undefined
+
+
+
+- [객체]
+*/
+
+/* 자바스크립트는 선언과 정의를 같이 함 */
+var u;
+var d = undefined
+console.log(u == d); // true
+
