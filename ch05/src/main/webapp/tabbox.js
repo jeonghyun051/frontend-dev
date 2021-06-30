30eef3e32a4307cd5248ad1113670adf77ba39dc
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-30 11:31:09 +0900

    basic

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,19 +1,21 @@
 var tabBox = {
 	init: function() {
-		window.addEventListener("load", this.onWindowLoad.bind(this));
+		console.log(1,this);
+		window.addEventListener("load", this._onWindowLoad.bind(this));
 	},
-	onWindowLoad: function() {
+	_onWindowLoad: function() {
 		var divTabBox = document.getElementsByClassName("tab-box")[0];
 		var ul = divTabBox.childNodes[1];
 		var liTabs = ul.getElementsByTagName("li");
 		
-		console.log(this);		
+		console.log(2,this);		
 		for(var i = 0; i < liTabs.length; i++){
-			liTabs[i].addEventListener("click", this.onTabClicked);
+			liTabs[i].addEventListener("click", this._onTabClicked);
 		}
 	},
-	onTabClicked: function() {
+	_onTabClicked: function() {
 		// unselected
+		console.log(3,this);
 		var lisSelected = document.getElementsByClassName("selected");
 		(lisSelected.length == 1) && (lisSelected[0].className = "");
 		
