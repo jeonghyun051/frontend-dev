b1786f599fdd6690272a53287c49008f24b3e986
jeonghyun051 <rlawjdgus051@naver.com>
2021-06-30 11:30:57 +0900

    basic

--- javascript-practice/basic/ex17.js
+++ javascript-practice/basic/ex17.js
@@ -15,6 +15,7 @@ f1();
 var o = {
     name: "마이콜"
 }
+// parameter 못넣는 함수 실행
 f1.apply(o);
 
 
@@ -22,4 +23,5 @@ console.log("===function 객체 메서드 call====")
 var f2 = function(s){
     console.log(s + " " + this.name);
 }
+// parameter 넣을 수 있고 실행 가능, 24라인 this는 o를 가르킴
 f2.call(o,"hi");
