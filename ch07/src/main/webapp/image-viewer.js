950541eecc8563983d90a44d5a795a76d99ae29d
jeonghyun051 <rlawjdgus051@naver.com>
2021-07-05 16:42:12 +0900

    imageviewer

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -6,15 +6,34 @@ var imageViewer = {
 		$(function() {
 			console.log("dom loaded:" + this);	// HTMLDocument
 			$("#btn-change").click(_this._changeImage.bind(_this));	// 여기서 bind를 해주면 changeImage 함수 this가 imageViewer object로 바뀜
+			$("#btn-slideshow").click(_this._slideImage.bind(_this));
 		});
 	},
 	_changeImage: function() {	// 이벤트 함수이기 때문에 여기서 this는 buttin
 		console.log("이벤트:" + this)	// HTMLButtonElements
 		// 랜덤 변수(0,1,2,3,4)
 		var result = Math.floor(Math.random() * this._images.length);
-		var data = this._images[result]
-		console.log("data:"+data);
+		var data = this._images[result];
+		$("#image").attr("src", "images/" + data.split(":", 2)[1]);
 	},
+	_slideImage: function() {
+		if (this.slide) {
+			clearTimeout(this.slide);
+			this.slide = null;
+			$("#btn-slideshow").text("슬라이드쇼 시작");
+		} else {
+			this._images.forEach((image, index) => {
+				this.slide = setTimeout(() => {
+					this._changeImage();
+/*					$("#image").attr("src", "images/" + image.split(":", 2)[1]);*/
+				},2000 * index);
+				}
+			)
+			console.log("this.slide:" + this.slide);
+			$("#btn-slideshow").text("슬라이드쇼 중지");
+		}
+	},
+	slide: false,
 	_images: [
 		"국화:Chrysanthemum.jpg",
 		"사막:Desert.jpg",
