15b6bcf824bdc1f70e9c89be39fa5d53ee8876f2
jeonghyun051 <rlawjdgus051@naver.com>
2021-07-06 10:51:59 +0900

    ajax xml, html, text

---
+++ ch08/src/main/java/com/douzone/frontdev/ch08/controller/ApiController.java
@@ -0,0 +1,30 @@
+package com.douzone.frontdev.ch08.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.frontdev.ch08.dto.XmlResult;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+
+	@ResponseBody
+	@RequestMapping("/text")
+	public String text() {
+		return "Text 데이터";
+	}	
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
+	}		
+	
+	@ResponseBody
+	@RequestMapping("/xml")
+	public Object xml() {
+		return XmlResult.success("Hello World");
+	}		
+}
